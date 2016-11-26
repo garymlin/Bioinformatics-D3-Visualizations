e4f11c227bb1b39f9993e32d04c392fab1392eeb
Gary Lin <garylinrule@gmail.com>
2016-11-25 18:56:44 -0800

    force-directed framework working

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ app.debug = True
 
 @app.route('/')
 def main():
-
     return render_template('index.html', api_data=[])
+
+@app.route('/fd')
+def fd():
+    return render_template('force-directed.html', api_data=[])
+
+@app.route('/miserables.json')
+def fd_json():
+    return render_template('miserables.json', api_data=[])
 
 
 if __name__ == "__main__":

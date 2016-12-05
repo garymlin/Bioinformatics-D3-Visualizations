d4855f548964947d35159ea2e9aa9c1cb9879f0b
James Xue <jamesxue100@gmail.com>
2016-12-05 02:45:38 -0500

    matrix working

--- tsv_to_json.py
+++ tsv_to_json.py
@@ -3,40 +3,52 @@ import json
 
 nodes = []
 drug_list = []
+pair_dict = {}
 effect_list = []
 
 links = []
 
 group_number = 1
 # num_lines = 0
-with open("offsides_first1500.tsv") as tsv:
+with open("twosides_every20_first50000.tsv") as tsv:
 	next(tsv)
 	for line in csv.reader(tsv, dialect="excel-tab"):
 		# num_lines += 1
 		# if num_lines % 50 == 0:
 
-		drug = line[1]
-		effect = line[3]
+		drug1 = line[2]
+		drug2 = line[3]
 		value = line[7]
-		if drug not in drug_list:
-			drug_list.append(drug)
-			drug_dict = {"id": drug, "group": group_number}
+		if drug1 not in drug_list:
+			drug_list.append(drug1)
+			drug_dict = {"name": drug1, "group": group_number}
+			pair_dict[drug1] = drug2
+			pair_dict[drug2] = drug1
 			nodes.append(drug_dict)
 			group_number += 1
-		if effect not in effect_list:
-			effect_list.append(effect)
-			effect_dict = {"id": effect, "group": group_number}
-			nodes.append(effect_dict)
-			group_number += 1
-		links_dict = {"source": drug, "target": effect, "value": value}
+
+with open("twosides_every20_first50000.tsv") as tsv:
+	next(tsv)
+	for line in csv.reader(tsv, dialect="excel-tab"):
+		drug1 = line[2]
+		drug2 = line[3]
+		value = line[7]
+
+		try:
+			drug1_index = drug_list.index(drug1)
+			drug2_index = drug_list.index(drug2)
+		except ValueError:
+			continue
+
+		links_dict = {"source": drug1_index, "target": drug2_index, "value": value}
 		if links_dict not in links:
 			links.append(links_dict)
 
 
 data = {"nodes": nodes, "links": links}
 
-with open('templates/drugs_effects_offsides_first1500.json', 'w') as outfile:
-    json.dump(data, outfile)
+with open('templates/drugs_effects_twosides_first50000.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4, separators=(',', ': '))
 
 
 

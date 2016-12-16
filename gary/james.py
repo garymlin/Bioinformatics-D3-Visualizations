f1ef3af9015ae2945b6c42b4d05ed740dd1d6709
Gary Lin <garylinrule@gmail.com>
2016-12-16 02:22:48 -0500

    yeet

--- gary/james.py
+++ gary/james.py
@@ -2,7 +2,7 @@ import csv
 import json
 
 
-version = "10000"
+version = "50000"
 
 nodes = []
 nodes_real = []
@@ -14,137 +14,180 @@ count = {}
 reaction_set = set()
 node_set = set()
 
-doc1 = "offsides_first" + version + ".json"
-json_data=open(doc1).read()
 
-data = json.loads(json_data)
-# print(link_list)
+# Create map for stitch_id to ATC
+stitch_to_ATC = {}
+with open("atc2stitch.csv") as csv_obj:
+  next(csv_obj)
+  for line in csv.reader(csv_obj, dialect="excel"):
+    if line[3] not in stitch_to_ATC:
+      stitch_to_ATC[line[3]] = line[0]
 
-### Data sanitization ###
-for reaction in data['nodes']:
-  node_set.add(reaction['id'])
-
-# print node_set
-
-for link in data['links']:
-  if ((link['source'] in node_set) and (link['target'] in node_set)):
-    if (float(link['value']) > 0.0001):
-      # print (link['value'])
-      # print (0.01)
-      # print(link['value'] > 0.01)
-      links.append(link)
-
-for link in links:
-  reaction_set.add(link["source"])
-  reaction_set.add(link["target"])
-
-# print reaction_set 
-
-for reaction in data['nodes']:
-  if (reaction['id'] in reaction_set):
-    nodes.append(reaction)
-
-for link in links:
-  if link["source"] in count:
-    val = count[link["source"]]
-    val = val + 1
-    count[link["source"]] = val
-  else:
-    count[link["source"]] = 1
-
-  if link["target"] in count:
-    val2 = count[link["target"]]
-    val2 = val2 + 1
-    count[link["target"]] = val2
-  else:
-    count[link["target"]] = 1
-
-for node in nodes:
-  if (float(count[node['id']]) > 1):
-    node['name'] = node['id']
-  nodes_real.append(node)
-
-
-
-event_to_umls = {}
-umls_to_physio = {}
-event_to_physio = {}
-physio_to_num = {}
+# Create map from drug to stitch_id
 drug_to_stitch = {}
-physio_num = 1
-
-
-
-# Adding physiological info to event data
-doc2 = "offsides_first" + version + ".tsv"
+doc2 = "twosides_every20_first" + version + ".tsv"
 with open(doc2) as tsv:
   next(tsv)
   for line in csv.reader(tsv, dialect="excel-tab"):
-    if line[3] not in event_to_physio:
-      event_to_umls[line[3]] = line[2]
-    if line[1] not in drug_to_stitch:
-      drug_to_stitch[line[1]] = line[0]
+      if line[2] not in drug_to_stitch:
+        drug_to_stitch[line[2]] = line[0]
+      if line[3] not in drug_to_stitch:
+        drug_to_stitch[line[3]] = line[1]
 
-with open("costart_categories.csv") as csv_obj:
-  next(csv_obj)
-  for line in csv.reader(csv_obj, dialect="excel"):
-    if line[0] not in umls_to_physio:
-      umls_to_physio[line[0]] = line[3]
-    if line[3] not in physio_to_num:
-      physio_to_num[line[3]] = physio_num
-      physio_num = physio_num + 1
 
-for event in event_to_umls:
-  umls = event_to_umls[event]
-  if umls in umls_to_physio:
-    event_to_physio[event] = umls_to_physio[umls]
 
-# print event_to_physio
 
+doc1 = "drugs_effects_twosides_first" + version + ".json"
+json_data=open(doc1).read()
+
+data = json.loads(json_data)
+
+hello ={}
 count = 0
-for node in nodes_real:
-  if node["id"] in event_to_physio:
-    node["physio"] = event_to_physio[node["id"]]
-    node["color_from_physio"] = physio_to_num[event_to_physio[node["id"]]]
-    count = count + 1
-  if node["id"] in drug_to_stitch:
-    node["display_id"] = node["id"]
-
-# print count
-
-for node in nodes_real:
-  if "display_id" in node:
-    nodes_final.append(node)
-  elif "physio" in node:
-    nodes_final.append(node)
-
-for node in nodes_final:
-  if "display_id" not in node:
-    node["display_id"] = node["physio"] + ": " + node["id"]
+for line in data['nodes']:
+  stitch = drug_to_stitch[line["name"]]
+  atc = stitch_to_ATC[stitch]
+  line["ATC"] = atc
+  nodes_final.append(line)
+# print nodes_final
 
 
-nodes_map = {}
-for node in nodes_final:
-  # print node["id"]
-  nodes_map[node["id"]] = "a"
+# doc1 = "offsides_first" + version + ".json"
+# json_data=open(doc1).read()
+
+# data = json.loads(json_data)
+# print(link_list)
+
+# ### Data sanitization ###
+# for reaction in data['nodes']:
+#   node_set.add(reaction['id'])
+
+# # print node_set
+
+# for link in data['links']:
+#   if ((link['source'] in node_set) and (link['target'] in node_set)):
+#     if (float(link['value']) > 0.0001):
+#       # print (link['value'])
+#       # print (0.01)
+#       # print(link['value'] > 0.01)
+#       links.append(link)
+
+# for link in links:
+#   reaction_set.add(link["source"])
+#   reaction_set.add(link["target"])
+
+# # print reaction_set 
+
+# for reaction in data['nodes']:
+#   if (reaction['id'] in reaction_set):
+#     nodes.append(reaction)
+
+# for link in links:
+#   if link["source"] in count:
+#     val = count[link["source"]]
+#     val = val + 1
+#     count[link["source"]] = val
+#   else:
+#     count[link["source"]] = 1
+
+#   if link["target"] in count:
+#     val2 = count[link["target"]]
+#     val2 = val2 + 1
+#     count[link["target"]] = val2
+#   else:
+#     count[link["target"]] = 1
+
+# for node in nodes:
+#   if (float(count[node['id']]) > 1):
+#     node['name'] = node['id']
+#   nodes_real.append(node)
 
 
-# for physio in physio_to_num:
-#   print physio
-#   mid = {}
-#   mid["id"] = physio
-#   mid["display_id"] = physio
-#   mid["color_from_physio"] = physio_to_num[physio]
-#   nodes_final.append(mid)
+
+# event_to_umls = {}
+# umls_to_physio = {}
+# event_to_physio = {}
+# physio_to_num = {}
+# drug_to_stitch = {}
+# physio_num = 1
 
 
-for link in links:
-  if link["source"] in nodes_map and link["target"] in nodes_map:
-    links_final.append(link)
-# print len(nodes_final)
+
+# # Adding physiological info to event data
+# doc2 = "offsides_first" + version + ".tsv"
+# with open(doc2) as tsv:
+#   next(tsv)
+#   for line in csv.reader(tsv, dialect="excel-tab"):
+#     if line[3] not in event_to_physio:
+#       event_to_umls[line[3]] = line[2]
+#     if line[1] not in drug_to_stitch:
+#       drug_to_stitch[line[1]] = line[0]
+
+# with open("atc2stitch.csv") as csv_obj:
+#   next(csv_obj)
+#   for line in csv.reader(csv_obj, dialect="excel"):
+#     if line[0] not in umls_to_physio:
+#       umls_to_physio[line[0]] = line[3]
+#     if line[3] not in physio_to_num:
+#       physio_to_num[line[3]] = physio_num
+#       physio_num = physio_num + 1
+
+# for event in event_to_umls:
+#   umls = event_to_umls[event]
+#   if umls in umls_to_physio:
+#     event_to_physio[event] = umls_to_physio[umls]
+
+# # print event_to_physio
+
+# count = 0
+# for node in nodes_real:
+#   if node["id"] in event_to_physio:
+#     node["physio"] = event_to_physio[node["id"]]
+#     node["color_from_physio"] = physio_to_num[event_to_physio[node["id"]]]
+#     count = count + 1
+#   if node["id"] in drug_to_stitch:
+#     node["display_id"] = node["id"]
+
+# # print count
+
+# for node in nodes_real:
+#   if "display_id" in node:
+#     nodes_final.append(node)
+#   elif "physio" in node:
+#     nodes_final.append(node)
+
+# for node in nodes_final:
+#   if "display_id" not in node:
+#     node["display_id"] = node["physio"] + ": " + node["id"]
+
+
+# nodes_map = {}
+# for node in nodes_final:
+#   # print node["id"]
+#   nodes_map[node["id"]] = "a"
+
+
+# # for physio in physio_to_num:
+# #   print physio
+# #   mid = {}
+# #   mid["id"] = physio
+# #   mid["display_id"] = physio
+# #   mid["color_from_physio"] = physio_to_num[physio]
+# #   nodes_final.append(mid)
+
+
+# for link in links:
+#   if link["source"] in nodes_map and link["target"] in nodes_map:
+#     links_final.append(link)
+# # print len(nodes_final)
 
 # print nodes
-data = {"nodes": nodes_final, "links": links_final}
+
+nodes_final = sorted(nodes_final, key=lambda k: k['ATC'])
+
+print nodes_final
+
+data = {"nodes": nodes_final, "links": data['links']}
 
 output = "final-" + version + ".json"
 with open(output, 'w') as outfile:
